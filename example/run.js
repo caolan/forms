815725a6c69469665e0f9404acbbe8a513a17b6b
Caolan McMahon <caolan@caolanmcmahon.com>
2010-06-14 22:09:43 +0100

    updated example to use form.handle

--- example/run.js
+++ example/run.js
@@ -10,62 +10,32 @@ var http = require('http'),
     parse = require('url').parse;
 
 
-// template for the page which displays the form
+// template for the example page
 var template = jsontemplate.Template(
     fs.readFileSync(__dirname + '/page.jsont').toString()
 );
 
 
 http.createServer(function(req, res){
-    // parse the url for pathname and query data
-    req.url = parse(req.url, true);
+    // create a new form object
+    var form = forms.create({
+        name:  forms.fields.string({required: true}),
+        email: forms.fields.string()
+    });
 
-    if(req.url.pathname == '/'){
-        // create a new form object
-        var form = forms.create({
-            name:  forms.fields.string({required: true}),
-            email: forms.fields.string()
-        });
-
-        /*
-        form.handle(req.url.query, {
-            success: function(data){
-                res.writeHead(302, {'Location': '/success'});
-                res.end();
-            },
-            // perhaps also have error and empty events
-            other: function(form){
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(template.expand({form: form.toHTML()}));
-            }
-        });
-        */
-
-        // if we got some data
-        if(req.url.query){
-            form.bind(req.url.query, function(err, form){
-                if(form.isValid()){
-                    // the form data is valid, redirect to success page!
-                    res.writeHead(302, {'Location': '/success'});
-                    res.end();
-                }
-                else {
-                    // return the html for the form:
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(template.expand({form: form.toHTML()}));
-                }
-            });
-        }
-        else {
-            // return the html for the form:
+    form.handle(req, {
+        success: function(data){
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(template.expand({form: form.toHTML()}));
+            res.write('<h1>Success!</h1>');
+            res.end('<pre>' + JSON.stringify(form.data) + '</pre>');
+        },
+        // perhaps also have error and empty events
+        other: function(form){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(template.expand({form: forms.render.div(form)}));
         }
-    }
-    else if(req.url.pathname == '/success'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>Form submitted!</h1>');
-    }
+    });
+
 }).listen(8080);
 
 sys.puts('Server running at http://127.0.0.1:8080/');
