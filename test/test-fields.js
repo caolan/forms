fae5fbe044fdc780fe0bdef7fd056e3d2c541d4f
Caolan McMahon <caolan@caolanmcmahon.com>
2010-06-19 20:01:24 +0100

    added email field

--- test/test-fields.js
+++ test/test-fields.js
@@ -5,19 +5,18 @@ var testField = function(field){
 
     exports[field + ' options'] = function(test){
         var fn1 = function(){return 'one'};
-        var fn2 = function(){return 'two'};
 
         var f = forms.fields[field]({
             required: true,
             label: 'test label',
-            validators: [fn1, fn2],
+            validators: [fn1],
             widget: 'some widget',
             choices: {one:'option one', two:'option two'}
         });
 
         test.equals(f.required, true);
         test.equals(f.label, 'test label');
-        test.same(f.validators, [fn1, fn2]);
+        test.equals(f.validators[f.validators.length-1], fn1);
         test.equals(f.widget, 'some widget');
         test.same(f.choices, {one:'option one', two:'option two'});
         test.equals(f.validate, undefined);
@@ -102,12 +101,14 @@ var testField = function(field){
     exports[field + ' validate required'] = function(test){
         test.expect(5);
         var f = forms.fields[field]({required: true});
+        f.validators = [];
         f.bind(undefined).validate('form', function(err, f){
             test.equals(f.value, undefined);
             test.equals(f.error, 'required field');
         });
         var f2 = forms.fields[field]({required: true});
         f2.parse = function(val){return val;};
+        f2.validators = [];
         f2.bind('val').validate('form', function(err, f2){
             test.equals(f2.value, 'val');
             test.equals(f2.data, 'val');
@@ -119,6 +120,7 @@ var testField = function(field){
     exports[field + ' validate no validators'] = function(test){
         test.expect(4);
         var f = forms.fields[field]();
+        f.validators = [];
         f.parse = function(data){
             test.equals(data, 'some data');
             return 'some data parsed';
@@ -203,6 +205,43 @@ exports['boolean toHTML'] = function(test){
             '<label for="id_fieldname">Fieldname</label>' +
             '<input type="checkbox" name="fieldname" id="id_fieldname" />' +
         '</div>'
+    );
+    test.done();
+};
+
+testField('email');
+
+exports['email parse'] = function(test){
+    test.equals(forms.fields.email().parse(), '');
+    test.equals(forms.fields.email().parse(null), '');
+    test.equals(forms.fields.email().parse(0), '0');
+    test.equals(forms.fields.email().parse(''), '');
+    test.equals(forms.fields.email().parse('some string'), 'some string');
+    test.done();
+};
+
+exports['email toHTML'] = function(test){
+    test.expect(3);
+    test.equals(
+        forms.fields.email().toHTML('fieldname'),
+        '<div class="field">' +
+            '<label for="id_fieldname">Fieldname</label>' +
+            '<input type="text" name="fieldname" id="id_fieldname" />' +
+        '</div>'
+    );
+    var f = forms.fields.email();
+    f.widget.toHTML = function(name, field){
+        test.equals(name, 'fieldname');
+        test.equals(field, f);
+        test.done();
+    };
+    f.toHTML('fieldname');
+};
+
+exports['email widget'] = function(test){
+    test.equals(
+        forms.fields.email().validators[0].toString(),
+        forms.validators.email().toString()
     );
     test.done();
 };
