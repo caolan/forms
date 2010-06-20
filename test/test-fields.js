15159670632c030eb385d0e0aa84bb00d6364e60
Caolan McMahon <caolan@caolanmcmahon.com>
2010-06-20 13:33:25 +0100

    added array field

--- test/test-fields.js
+++ test/test-fields.js
@@ -1,4 +1,5 @@
-var forms = require('forms');
+var forms = require('forms'),
+    fields = forms.fields;
 
 
 var testField = function(field){
@@ -6,7 +7,7 @@ var testField = function(field){
     exports[field + ' options'] = function(test){
         var fn1 = function(){return 'one'};
 
-        var f = forms.fields[field]({
+        var f = fields[field]({
             required: true,
             label: 'test label',
             validators: [fn1],
@@ -26,7 +27,7 @@ var testField = function(field){
     exports[field + ' bind'] = function(test){
         test.expect(7);
 
-        var f = forms.fields[field]({
+        var f = fields[field]({
             label: 'test label',
             validators: [
                 function(form, field, callback){
@@ -51,7 +52,7 @@ var testField = function(field){
     exports[field + ' validate'] = function(test){
         test.expect(10);
 
-        var f = forms.fields[field]({
+        var f = fields[field]({
             label: 'test label',
             validators: [
                 function(form, field, callback){
@@ -83,7 +84,7 @@ var testField = function(field){
 
     exports[field + ' validate empty'] = function(test){
         test.expect(1);
-        var f = forms.fields[field]({
+        var f = fields[field]({
             validators: [function(form, field, callback){
                 test.ok(false, 'validators should not be called');
                 callback('some error');
@@ -100,13 +101,13 @@ var testField = function(field){
 
     exports[field + ' validate required'] = function(test){
         test.expect(5);
-        var f = forms.fields[field]({required: true});
+        var f = fields[field]({required: true});
         f.validators = [];
         f.bind(undefined).validate('form', function(err, f){
             test.equals(f.value, undefined);
             test.equals(f.error, 'required field');
         });
-        var f2 = forms.fields[field]({required: true});
+        var f2 = fields[field]({required: true});
         f2.parse = function(val){return val;};
         f2.validators = [];
         f2.bind('val').validate('form', function(err, f2){
@@ -119,7 +120,7 @@ var testField = function(field){
 
     exports[field + ' validate no validators'] = function(test){
         test.expect(4);
-        var f = forms.fields[field]();
+        var f = fields[field]();
         f.validators = [];
         f.parse = function(data){
             test.equals(data, 'some data');
@@ -137,24 +138,24 @@ var testField = function(field){
 testField('string');
 
 exports['string parse'] = function(test){
-    test.equals(forms.fields.string().parse(), '');
-    test.equals(forms.fields.string().parse(null), '');
-    test.equals(forms.fields.string().parse(0), '0');
-    test.equals(forms.fields.string().parse(''), '');
-    test.equals(forms.fields.string().parse('some string'), 'some string');
+    test.equals(fields.string().parse(), '');
+    test.equals(fields.string().parse(null), '');
+    test.equals(fields.string().parse(0), '0');
+    test.equals(fields.string().parse(''), '');
+    test.equals(fields.string().parse('some string'), 'some string');
     test.done();
 };
 
 exports['string toHTML'] = function(test){
     test.expect(3);
     test.equals(
-        forms.fields.string().toHTML('fieldname'),
+        fields.string().toHTML('fieldname'),
         '<div class="field">' +
             '<label for="id_fieldname">Fieldname</label>' +
             '<input type="text" name="fieldname" id="id_fieldname" />' +
         '</div>'
     );
-    var f = forms.fields.string();
+    var f = fields.string();
     f.widget.toHTML = function(name, field){
         test.equals(name, 'fieldname');
         test.equals(field, f);
@@ -167,17 +168,17 @@ exports['string toHTML'] = function(test){
 testField('number');
 
 exports['number parse'] = function(test){
-    test.ok(isNaN(forms.fields.number().parse()));
-    test.ok(isNaN(forms.fields.number().parse(null)));
-    test.equals(forms.fields.number().parse(0), 0);
-    test.ok(isNaN(forms.fields.number().parse('')));
-    test.equals(forms.fields.number().parse('123'), 123);
+    test.ok(isNaN(fields.number().parse()));
+    test.ok(isNaN(fields.number().parse(null)));
+    test.equals(fields.number().parse(0), 0);
+    test.ok(isNaN(fields.number().parse('')));
+    test.equals(fields.number().parse('123'), 123);
     test.done();
 };
 
 exports['number toHTML'] = function(test){
     test.equals(
-        forms.fields.number().toHTML('fieldname'),
+        fields.number().toHTML('fieldname'),
         '<div class="field">' +
             '<label for="id_fieldname">Fieldname</label>' +
             '<input type="text" name="fieldname" id="id_fieldname" />' +
@@ -189,18 +190,18 @@ exports['number toHTML'] = function(test){
 testField('boolean');
 
 exports['boolean parse'] = function(test){
-    test.equals(forms.fields.boolean().parse(), false);
-    test.equals(forms.fields.boolean().parse(null), false);
-    test.equals(forms.fields.boolean().parse(0), false);
-    test.equals(forms.fields.boolean().parse(''), false);
-    test.equals(forms.fields.boolean().parse('on'), true);
-    test.equals(forms.fields.boolean().parse('true'), true);
+    test.equals(fields.boolean().parse(), false);
+    test.equals(fields.boolean().parse(null), false);
+    test.equals(fields.boolean().parse(0), false);
+    test.equals(fields.boolean().parse(''), false);
+    test.equals(fields.boolean().parse('on'), true);
+    test.equals(fields.boolean().parse('true'), true);
     test.done();
 };
 
 exports['boolean toHTML'] = function(test){
     test.equals(
-        forms.fields.boolean().toHTML('fieldname'),
+        fields.boolean().toHTML('fieldname'),
         '<div class="field">' +
             '<label for="id_fieldname">Fieldname</label>' +
             '<input type="checkbox" name="fieldname" id="id_fieldname" />' +
@@ -213,28 +214,28 @@ testField('email');
 
 exports['email parse'] = function(test){
     test.equals(
-        forms.fields.email().parse.toString(),
-        forms.fields.string().parse.toString()
+        fields.email().parse.toString(),
+        fields.string().parse.toString()
     );
     test.done();
 };
 
 exports['email toHTML'] = function(test){
     test.equals(
-        forms.fields.email().toHTML.toString(),
-        forms.fields.string().toHTML.toString()
+        fields.email().toHTML.toString(),
+        fields.string().toHTML.toString()
     );
     test.done();
 };
 
 exports['email validators'] = function(test){
     test.equals(
-        forms.fields.email().validators[0].toString(),
+        fields.email().validators[0].toString(),
         forms.validators.email().toString()
     );
     var fn1 = function(){return 'one';};
     var fn2 = function(){return 'two';};
-    var f = forms.fields.email({validators: [fn1, fn2]});
+    var f = fields.email({validators: [fn1, fn2]});
     test.equals(
         f.validators[0].toString(),
         forms.validators.email().toString()
@@ -247,16 +248,16 @@ testField('password');
 
 exports['password parse'] = function(test){
     test.equals(
-        forms.fields.password().parse.toString(),
-        forms.fields.string().parse.toString()
+        fields.password().parse.toString(),
+        fields.string().parse.toString()
     );
     test.done();
 };
 
 exports['password toHTML'] = function(test){
     test.equals(
-        forms.fields.password().toHTML.toString(),
-        forms.fields.string().toHTML.toString()
+        fields.password().toHTML.toString(),
+        fields.string().toHTML.toString()
     );
     test.done();
 };
@@ -265,32 +266,54 @@ testField('url');
 
 exports['url parse'] = function(test){
     test.equals(
-        forms.fields.url().parse.toString(),
-        forms.fields.string().parse.toString()
+        fields.url().parse.toString(),
+        fields.string().parse.toString()
     );
     test.done();
 };
 
 exports['url toHTML'] = function(test){
     test.equals(
-        forms.fields.url().toHTML.toString(),
-        forms.fields.string().toHTML.toString()
+        fields.url().toHTML.toString(),
+        fields.string().toHTML.toString()
     );
     test.done();
 };
 
 exports['url validators'] = function(test){
     test.equals(
-        forms.fields.url().validators[0].toString(),
+        fields.url().validators[0].toString(),
         forms.validators.url().toString()
     );
     var fn1 = function(){return 'one';};
     var fn2 = function(){return 'two';};
-    var f = forms.fields.url({validators: [fn1, fn2]});
+    var f = fields.url({validators: [fn1, fn2]});
     test.equals(
         f.validators[0].toString(),
         forms.validators.url().toString()
     );
     test.same(f.validators.slice(1), [fn1, fn2]);
+    test.done();
+};
+
+testField('array');
+
+exports['array parse'] = function(test){
+    test.same(fields.array().parse(), []);
+    test.same(fields.array().parse(null), [null]);
+    test.same(fields.array().parse(0), [0]);
+    test.same(fields.array().parse(''), ['']);
+    test.same(fields.array().parse('abc'), ['abc']);
+    test.same(
+        fields.array().parse(['one','two','three']), ['one','two','three']
+    );
+    test.done();
+};
+
+exports['array toHTML'] = function(test){
+    test.equals(
+        fields.array().toHTML.toString(),
+        fields.string().toHTML.toString()
+    );
     test.done();
 };
