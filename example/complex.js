6921a9eca002f2e1edf5d65a47c051820f3dccc2
Caolan McMahon <caolan@caolanmcmahon.com>
2010-06-21 18:28:43 +0100

    updated complex form example

--- example/complex.js
+++ example/complex.js
@@ -19,56 +19,55 @@ var template = jsontemplate.Template(
 );
 
 
+var form = forms.create({
+    name:  fields.string({required: true}),
+    email: fields.email({required: true, label: 'Email Address'}),
+    website: fields.url(),
+    password: fields.password({required: true}),
+    password_confirm: fields.password({
+        required: true,
+        validators: [validators.matchField('password')]
+    }),
+    options: fields.string({
+        choices: {
+            one: 'option one',
+            two: 'option two',
+            three: 'option three'
+        },
+        widget: widgets.select(),
+        validators: [function(form, field, callback){
+            if(field.data === 'two') callback('two?! are you crazy?!');
+            else callback();
+        }]
+    }),
+    more_options: fields.array({
+        choices: {one: 'item 1', two: 'item 2', three: 'item 3'},
+        widget: widgets.multipleCheckbox()
+    }),
+    even_more: fields.string({
+        choices: {one: 'item 1', two: 'item 2', three: 'item 3'},
+        widget: widgets.multipleRadio()
+    }),
+    and_more: fields.array({
+        choices: {one: 'item 1', two: 'item 2', three: 'item 3'},
+        widget: widgets.multipleSelect()
+    }),
+    notes: fields.string({
+        widget: widgets.textarea({rows: 6})
+    }),
+    spam_me: fields.boolean()
+});
+
+
 http.createServer(function(req, res){
-    // create a new form object
-    var form = forms.create({
-        name:  fields.string({required: true}),
-        email: fields.email({required: true, label: 'Email Address'}),
-        website: fields.url(),
-        password: fields.password({required: true}),
-        password_confirm: fields.password({
-            required: true,
-            validators: [validators.matchField('password')]
-        }),
-        options: fields.string({
-            choices: {
-                one: 'option one',
-                two: 'option two',
-                three: 'option three'
-            },
-            widget: widgets.select(),
-            validators: [function(form, field, callback){
-                if(field.data === 'two') callback('two?! are you crazy?!');
-                else callback();
-            }]
-        }),
-        more_options: fields.array({
-            choices: {one: 'item 1', two: 'item 2', three: 'item 3'},
-            widget: widgets.multipleCheckbox()
-        }),
-        even_more: fields.string({
-            choices: {one: 'item 1', two: 'item 2', three: 'item 3'},
-            widget: widgets.multipleRadio()
-        }),
-        and_more: fields.array({
-            choices: {one: 'item 1', two: 'item 2', three: 'item 3'},
-            widget: widgets.multipleSelect()
-        }),
-        notes: fields.string({
-            widget: widgets.textarea({rows: 6})
-        }),
-        spam_me: fields.boolean()
-    });
 
     form.handle(req, {
         success: function(form){
             var req_data = require('url').parse(req.url,1).query;
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write('<h1>Success!</h1>');
-            res.write('<pre>' + sys.inspect(form.data) + '</pre>');
-            res.end('<pre>' + sys.inspect(req_data) + '</pre>');
+            res.end('<pre>' + sys.inspect(form.data) + '</pre>');
         },
-        // perhaps also have error and empty events
         other: function(form){
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(template.expand({form: form.toHTML()}));
