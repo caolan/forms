067d3a286f4e689df80dec5510ada5d27e23b516
Michael Mior <mike@joinbunch.com>
2012-10-27 16:12:11 -0400

    Fix test bugs, add Travis CI, remove submodules
    Dependencies are now managed via package.json instead of submodules.
    This simplifies things and allows tests to be run via Travis CI.

--- test.js
+++ test.js
@@ -1,22 +1,4 @@
 #!/usr/local/bin/node
 
-// require.paths.push(__dirname);
-// require.paths.push(__dirname + '/deps');
-// require.paths.push(__dirname + '/lib');
-/* Note: require.paths is removed in node v0.5 and later. Use 'NODE_PATH=`pwd`:`pwd`/deps/:`pwd`/lib/ node test.js' to run tests. */
-
-try {
-    var testrunner = require('nodeunit').testrunner;
-} catch(e) {
-    var util = require('util');
-    util.puts("Cannot find nodeunit module.");
-    util.puts("You can download submodules for this project by doing:");
-    util.puts("");
-    util.puts("    git submodule init");
-    util.puts("    git submodule update");
-    util.puts("");
-    process.exit();
-}
-
-process.chdir(__dirname);
-testrunner.run(['test']);
+var reporter = require('nodeunit').reporters.default;
+reporter.run(['test']);
