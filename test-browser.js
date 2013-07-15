9dd7361164a498cfddea7094b9b472d75591a7a5
Jordan Harband <ljharb@gmail.com>
2013-07-15 09:27:55 -0700

    "use strict" should always be inside a function.

--- test-browser.js
+++ test-browser.js
@@ -1,12 +1,15 @@
-"use strict";
+(function () {
+    "use strict";
 
-var reporter = require('nodeunit-browser-tap');
+    var reporter = require('nodeunit-browser-tap');
 
-reporter.run({
-  fields: require('./test/test-fields'),
-  // form: require('./test/test-form'), disabled until server-specific tests are split out
-  forms: require('./test/test-forms'),
-  render: require('./test/test-render'),
-  // validators: require('./test/test-validators'), disabled until this unicode fix is merged: https://github.com/substack/testling/pull/35
-  widgets: require('./test/test-widgets')
-});
+    reporter.run({
+        fields: require('./test/test-fields'),
+        // form: require('./test/test-form'), disabled until server-specific tests are split out
+        forms: require('./test/test-forms'),
+        render: require('./test/test-render'),
+        // validators: require('./test/test-validators'), disabled until this unicode fix is merged: https://github.com/substack/testling/pull/35
+        widgets: require('./test/test-widgets')
+    });
+)()};
+
