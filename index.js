9f118a89cf5c42e9013beeff89ba898652958e52
Jordan Harband <ljharb@gmail.com>
2021-12-09 14:14:03 -0800

    [meta] point "main" directly to the right file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// This file is just added for convenience so this repository can be
-// directly checked out into a project's deps folder
+// TODO: remove, semver-major
 module.exports = require('./lib/forms');
 
